3d8707b8d8198bd66f6fd03b6e58a892b725f8bb
Morten Mjelva <morten.mjelva@gmail.com>
2017-09-09 12:41:15 +0200

    Updated matlab model

--- Models/Matlab/InertialMatlab.m
+++ Models/Matlab/InertialMatlab.m
@@ -3,9 +3,10 @@ A = [0 0 1 0;
      0 0 0 1;
      -c1/J1 c1/J1 -d1/J1 d1/J1; 
      c1/J2 -(c1+c2)/J2 d1/J2 -(d1+d2)/J2];
+B = zeros(size(A,1),1);
 C = [0 0 1 0];
 
-sys  = ss(A, zeros(size(A,1),1), C, 0);
+sys  = ss(A, B, C, 0);
 init = [0 1 0 0];
 
-initial(sys, x0);
+initial(sys, init);
