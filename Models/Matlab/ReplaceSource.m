769442dbaf6e3dd6e4e039503b6cfc3054a9ba25
Morten Mjelva <morten.mjelva@gmail.com>
2018-01-08 10:13:01 +0100

    Writing

--- Models/Matlab/ReplaceSource.m
+++ Models/Matlab/ReplaceSource.m
@@ -2,5 +2,3 @@ load_system('InertialSimulink');
 replace_block('InertialSimulink', 'Step', 'Sin', 'noprompt');
 set_param('InertialSimulink/Reference', 'Amplitude', '2');
 set_param('InertialSimulink/Reference', 'Frequency', '10');
-
-replace_block('InertialSimulink', 'Sin', 'Step', 'noprompt');
